ca6c9109c4c8e5a9039fde219aad8f39a85f5a7c
Sparsh <117457293+sparsh1119@users.noreply.github.com>
2024-12-05 01:03:57 +0530

    REST Api, Spring Boot controller CRUD opertion using collections

---
+++ jornalApplication/src/main/java/com/JornalApp/jornalApplication/JornalApplication.java
@@ -0,0 +1,13 @@
+package com.JornalApp.jornalApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JornalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JornalApplication.class, args);
+	}
+
+}
---
+++ jornalApplication/src/main/java/com/JornalApp/jornalApplication/controller/Jornalentry.java
@@ -0,0 +1,44 @@
+package com.JornalApp.jornalApplication.controller;
+
+import com.JornalApp.jornalApplication.entity.Entries;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+@RequestMapping("/jornal")
+public class Jornalentry {
+
+    private HashMap<Long,Entries> jornalEntry = new HashMap<>();
+
+    @GetMapping
+    public List<Entries> getAll(){
+        return new ArrayList<>(jornalEntry.values());
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody Entries myEntry){
+        jornalEntry.put(myEntry.getId(),myEntry);
+        return  true;
+    }
+
+
+    @GetMapping("/id/{myid}")
+    public Entries getOne(@PathVariable long myid){
+        return jornalEntry.get(myid);
+    }
+
+    @PutMapping("/update/{myid}")
+    public boolean updateEntry(@PathVariable Long myid,@RequestBody Entries myEntry){
+         jornalEntry.put(myid,myEntry);
+         return true;
+    }
+
+    @DeleteMapping("/dlt/{myid}")
+    public boolean remove(@PathVariable long myid){
+        jornalEntry.remove(myid);
+        return true;
+    }
+}
